5dcc2a913812e55eb29c7d410431968c4b2f0221
Gajendrasuman <Gajendrasuman868@gmail.com>
2023-07-11 11:41:44 +0000

    s

---
+++ nav.js
@@ -0,0 +1,8 @@
+var navs = document.querySelector('.nav-icon');
+var spans = document.querySelector('.nav-icon span');
+navs.onclick = ()=>{
+    alert(2);
+}
+spans.onclick = ()=>{
+    alert(3);
+} 
