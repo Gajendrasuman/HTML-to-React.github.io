ae72ca2475bb597b94e7d3b76d3972e83f52b1cc
Gajendrasuman <Gajendrasuman868@gmail.com>
2023-07-12 14:46:10 +0000

    s

--- nav.js
+++ nav.js
@@ -26,3 +26,26 @@ window.onresize = () => {
         z = 0
     }
 }
+
+CurrentScrollPos = window.scrollY
+window.onscroll = ()=>{
+    if(window.scrollY === 0){
+        document.querySelector('nav').style = "background: transparent;\
+        border-bottom: none; position: none; box-shadow: none;"  
+        CurrentScrollPos = window.scrollY 
+    }
+    else if(window.scrollY < CurrentScrollPos){
+        document.querySelector('nav').style = "top:-8rem; position: fixed;";
+        CurrentScrollPos = window.scrollY
+    }
+    else if(window.scrollY > CurrentScrollPos){
+        document.querySelector('nav').style = "background: #fff; \
+        border-bottom: 0.1rem solid var(--primary); position: fixed;\
+        top:0; left:0;box-shadow: 0 0 3px 1px var(--primary);\
+        ";
+        CurrentScrollPos = window.scrollY
+    }
+
+    
+
+}
