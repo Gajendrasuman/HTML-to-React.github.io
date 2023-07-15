a34ae8fda238d8410d700c4049f984fb41e7a9c5
Gajendrasuman <Gajendrasuman868@gmail.com>
2023-07-15 13:59:50 +0000

    s

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const dots = document.querySelectorAll('.dots')
 function slide(n){
     //dots[n]
     if(n==0){
-        container[n].setAttribute("class",'container active-slide-first')
-        container[1].setAttribute('class', 'container')
-        container[2].setAttribute('class', 'container')
+        container[n].style.display = "grid"
+        container[1].style.display = "none"
+        container[2].style.display = "none"
     }
     else if(n==1){
-        container[n].setAttribute("class",'container active-slide-second')
-        container[0].setAttribute('class', 'container')
-        container[2].setAttribute('class', 'container')
+        container[n].style.display = "grid"
+        container[0].style.display = "none"
+        container[2].style.display = "none"
     }
     else{
-        container[n].setAttribute("class",'container active-slide')
-        container[0].setAttribute('class', 'container')
-        container[1].setAttribute('class', 'container')
+        container[n].style.display = "grid"
+        container[1].style.display = "none"
+        container[0].style.display = "none"
     }
 }
 //setInterval(()=>{
